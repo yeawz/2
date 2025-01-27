2d5f41e581dff61e841862c15ef6911538c9b3fa
Горелова Екатерина ЗП-41 <katya.gorelova.18@gmail.com>
2025-01-27 21:00:58 +0700

    Добавлен метод getCarByBrendAndYearOperational в класс Main

--- Car.java
+++ Car.java
@@ -1,14 +1,12 @@
 public class Car {
+    private int id;
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private double price;
+    private String registrationNumber;
 
-    private int id;             // Идентификатор автомобиля
-    private String brand;       // Марка автомобиля
-    private String model;       // Модель автомобиля
-    private int year;           // Год выпуска автомобиля
-    private String color;       // Цвет автомобиля
-    private double price;       // Цена автомобиля
-    private String registrationNumber; // Регистрационный номер автомобиля
-
-     // Конструктор, принимающий значения полей в качестве параметров
     public Car(int id, String brand, String model, int year, String color, double price, String registrationNumber) {
         this.id = id;
         this.brand = brand;
@@ -19,76 +17,72 @@ public class Car {
         this.registrationNumber = registrationNumber;
     }
 
-    // Геттеры (нестатические)
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getBrand() {
         return brand;
     }
 
-    public String getModel() {
-        return model;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
-    }
-
-    // Сеттеры (нестатические)
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public void setBrand(String brand) {
         this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
     }
 
     public void setModel(String model) {
         this.model = model;
     }
 
+    public int getYear() {
+        return year;
+    }
+
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public String getColor() {
+        return color;
     }
 
     public void setColor(String color) {
         this.color = color;
     }
 
+    public double getPrice() {
+        return price;
+    }
+
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
     }
 
     public void setRegistrationNumber(String registrationNumber) {
         this.registrationNumber = registrationNumber;
     }
 
-   // Переопределенный метод toString()
-  @Override
-  public String toString() {
-    return "Car{" +
-      "id=" + id +
-      ", brand='" + brand + '\'' +
-      ", model='" + model + '\'' +
-      ", year=" + year +
-      ", color='" + color + '\'' +
-      ", price=" + price +
-      ", registrationNumber='" + registrationNumber + '\'' +
-     '}';
-  }
- }
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                '}';
+    }
+}
--- Main.java
+++ Main.java
@@ -1,58 +1,64 @@
+import java.time.Year;
 public class Main {
-
+    
     public static void main(String[] args) {
-       Car[] cars = {
-           new Car(1, "Toyota", "Camry", 2022, "Red", .4560000, "ET123C"),
-           new Car(2, "Honda", "Civic", 2021, "Blue", 220000.0, "EH456E"),
-           new Car(3, "Toyota", "Corolla", 2023, "Silver", .654000, "EC789K"),
-           new Car(4, "Nissan", "Altima", 2022, "Black", .380000, "MB012OP")
-      };
+        Car[] cars = {
+                new Car(1, "Toyota", "Camry", 2022, "Red", 6468770.0, "CC123C"),
+                new Car(2, "Honda", "Civic", 2018, "Blue", 22688000.0, "EE456E"),
+                new Car(3, "Toyota", "Corolla", 2020, "Silver", 16723000.0, "MM789M"),
+                new Car(4, "Nissan", "Altima", 2019, "Black", 1987510.0, "PP012P"),
+                new Car(5, "Toyota", "RAV4", 2015, "White", 87876700.0, "TT345T")
+        };
 
-       String searchBrand = "Toyota";
-      Car[] toyotaCars = getCarByBrend(cars, searchBrand);
+        String searchBrand = "Toyota";
+        int operationalYears = 3;
 
-       if(toyotaCars.length == 0){
-        System.out.println("No cars of brand '" + searchBrand + "' found.");
-      } else {
-        System.out.println("Cars of brand '" + searchBrand + "':");
-       for (Car car : toyotaCars) {
-          System.out.println(car);
-       }
-     }
+        Car[] filteredCars = getCarByBrendAndYearOperational(cars, searchBrand, operationalYears);
 
-      String searchBrand2 = "BMW";
-    Car[] bmwCars = getCarByBrend(cars, searchBrand2);
-
-    if(bmwCars.length == 0){
-      System.out.println("No cars of brand '" + searchBrand2 + "' found.");
-    } else {
-      System.out.println("Cars of brand '" + searchBrand2 + "':");
-      for (Car car : bmwCars) {
-        System.out.println(car);
-    }
-  }
-}
-
-
- public static Car[] getCarByBrend(Car[] cars, String brand) {
-   if (cars == null || cars.length == 0 || brand == null || brand.isEmpty()) {
-       return new Car[0]; // Возвращаем пустой массив, если входные данные некорректны
-     }
-
-    int count = 0;
-    for (Car car : cars) {
-      if (car != null && brand.equals(car.getBrand())) {
-        count++;
-     }
-   }
-
-    Car[] result = new Car[count];
-    int index = 0;
-    for (Car car : cars) {
-       if (car != null && brand.equals(car.getBrand())) {
-        result[index++] = car;
+        if (filteredCars.length == 0) {
+            System.out.println("No cars of brand '" + searchBrand + "' operational more than " + operationalYears + " years were found.");
+        } else {
+            System.out.println("Cars of brand '" + searchBrand + "' operational more than " + operationalYears + " years:");
+            for(Car car: filteredCars) {
+                System.out.println(car);
+            }
         }
-   }
-   return result;
-  }
+
+        String searchBrand2 = "Honda";
+        int operationalYears2 = 5;
+
+        Car[] filteredCars2 = getCarByBrendAndYearOperational(cars, searchBrand2, operationalYears2);
+
+        if (filteredCars2.length == 0) {
+            System.out.println("No cars of brand '" + searchBrand2 + "' operational more than " + operationalYears2 + " years were found.");
+        } else {
+            System.out.println("Cars of brand '" + searchBrand2 + "' operational more than " + operationalYears2 + " years:");
+            for(Car car: filteredCars2) {
+                System.out.println(car);
+            }
+        }
+    }
+
+    public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brand, int years) {
+        if (cars == null || cars.length == 0 || brand == null || brand.isEmpty() || years < 0) {
+            return new Car[0];  // Возвращаем пустой массив в случае некорректных входных данных
+        }
+
+        int currentYear = Year.now().getValue();
+        int count = 0;
+        for(Car car : cars) {
+            if (car != null && brand.equals(car.getBrand()) && (currentYear - car.getYear()) > years) {
+                count++;
+            }
+        }
+
+        Car[] result = new Car[count];
+        int index = 0;
+        for (Car car : cars) {
+            if (car != null && brand.equals(car.getBrand()) && (currentYear - car.getYear()) > years) {
+                result[index++] = car;
+            }
+        }
+        return result;
+    }
 }
